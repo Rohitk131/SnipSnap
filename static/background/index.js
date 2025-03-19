54b991f3aa4824d41b469522541bb85c5c0227d7
rohitk131 <rohitkumar13103@gmail.com>
2025-03-19 23:01:42 +0530

    added fixed code

---
+++ static/background/index.js
@@ -0,0 +1,8 @@
+// ...existing code...
+
+// Replace this line:
+// import or loading of "https://apis.google.com/js/api.js"
+// With this:
+importScripts('../vendor/google-api.js');
+
+// ...existing code...
---
+++ static/vendor/google-api.js
@@ -0,0 +1,10 @@
+// This file contains the Google API client library code
+// Downloaded from https://apis.google.com/js/api.js
+
+// Google API client library code goes here
+// You should download the minified content from https://apis.google.com/js/api.js
+// and paste it here
+
+// For example, the file might start with:
+var gapi=window.gapi=window.gapi||{};gapi._bs=new Date().getTime();
+// ... rest of the API code ...
